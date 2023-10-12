3ee06adf6fe3d810fff6a772d167d342e56712fa
intredd <srv.intredd@gmail.com>
2023-10-12 11:26:54 +0200

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,23 @@
 const placeContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
 initialCards.forEach(element => {
-  placeContainer.append(addCard(element['name'], element['link'], removeCard));
+  element['alt'] = 'Фотография ' + element['name'];
+  console.log(element['alt']);
+  placeContainer.append(addCard(element['name'], element['link'], element['alt'], removeCard));
 });
 
-function addCard(name, link, removeCallback) {
-  const cardTemplate = document.querySelector('#card-template').content;
+function addCard(name, link, alt, removeCallback) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = name;
   cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = alt;
   cardElement.querySelector('.card__delete-button').addEventListener('click', removeCallback);
 
   return cardElement;
 }
 
 function removeCard(event) {
-  event.target.parentElement.remove();
+  event.target.closest('.card').remove();
 }
